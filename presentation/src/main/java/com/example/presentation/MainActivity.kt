2e35ce7271c12c9f6e3ad72db6dfabd48661edf2
Magzhan Nurbapa <magzhan270819@gmail.com>
2024-05-12 10:52:46 +0500

    presentation module

--- presentation/src/main/java/com/example/presentation/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/MainActivity.kt
@@ -1,20 +1,29 @@
-package com.example.animequote
+package com.example.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.presentation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val viewModel = (application as ProvideViewModel).viewModel()
+
+        viewModel.liveData.observe(this) {
+            binding.mainTextView.text = it
+        }
+        viewModel.load()
     }
 }
